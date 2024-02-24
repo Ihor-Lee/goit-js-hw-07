9fb354db6282594b2a89884aa9b703f58eaf2c7d
Ihor-Lee <Dr.Greezlee@gmail.com>
2024-02-24 13:52:33 +0200

    Finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,46 +1,19 @@
 "use strict";
-const getUserNames = (users) => {
-return users.map(user =>user.name)
- }
+const categoriesList = document.getElementById('categories');
+
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+
+console.log(`Number of categories: ${categoryItems.length}`);
 
 
 
-console.log(
-  getUserNames([
-    {
-      name: "Moore Hensley",
-      email: "moorehensley@indexia.com",
-      balance: 2811,
-    },
-    {
-      name: "Sharlene Bush",
-      email: "sharlenebush@tubesys.com",
-      balance: 3821,
-    },
-    {
-      name: "Ross Vazquez",
-      email: "rossvazquez@xinware.com",
-      balance: 3793,
-    },
-    {
-      name: "Elma Head",
-      email: "elmahead@omatom.com",
-      balance: 2278,
-    },
-    {
-      name: "Carey Barr",
-      email: "careybarr@nurali.com",
-      balance: 3951,
-    },
-    {
-      name: "Blackburn Dotson",
-      email: "blackburndotson@furnigeer.com",
-      balance: 1498,
-    },
-    {
-      name: "Sheree Anthony",
-      email: "shereeanthony@kog.com",
-      balance: 2764,
-    },
-  ])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+categoryItems.forEach((categoryItem) => {
+
+  const categoryName = categoryItem.querySelector('h2').textContent;
+
+  const subCategoryItems = categoryItem.querySelectorAll('li');
+
+  console.log(`category: ${categoryName}`);
+  console.log(`elements: ${categoryName} ${subCategoryItems.length}`);
+});
